35ddfc602d6be68d33816cefed9e5127d2dc691d
wadosmangprail@gmail.com <wadosmangprail@gmail.com>
2020-07-23 10:23:55 +0100

    Git 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import {searchUser} from './reducer'
+import 'tachyons'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const stor = createStore(searchUser)
+
+ReactDOM.render(<Provider store={stor}>
+                    <App />
+                </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

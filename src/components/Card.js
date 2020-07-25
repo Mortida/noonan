c1280a70859feddd3eb0914016a5cb3649ea11ed
Murtada <43283994+Mortida@users.noreply.github.com>
2020-07-25 09:42:30 +0100

    git 4

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ id, name, email }) => {
   return (
     <div className='tc grow bg-light-blue br3 pa3 ma2 dib bw2 shadow-5' >
-      <img style={{ width: '280px', height: '476.83px' }} src={`https://github.com/Mortida/noonan/blob/moro-feature/public/images/image${id}.jpg`} alt='' />
+      <img style={{ width: '280px', height: '476.83px' }} src={`https://github.com/Mortida/noonan/blob/moro-feature/public/images/image${id}.jpg?raw=true`} alt='' />
         <h4>{email}</h4>
         <p>{name}</p>
     </div>

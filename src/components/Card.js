057380e5e16d99b61cbd7700bca6713654e95d51
wadosmangprail@gmail.com <wadosmangprail@gmail.com>
2020-07-25 08:48:49 +0100

    git 2

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ id, name, email }) => {
   return (
     <div className='tc grow bg-light-blue br3 pa3 ma2 dib bw2 shadow-5' >
-        <img style={{width: '280px', height:'476.83px'}} src={`../images/image${id}.jpg`} alt='' />
+        <img style={{width: '280px', height:'476.83px'}} src={`/images/image${id}.jpg`} alt='' />
         <h4>{email}</h4>
         <p>{name}</p>
     </div>
